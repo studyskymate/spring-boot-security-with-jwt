4040ffcd0cd6b4d803f9fd4efd0a986c38c98d66
DINESH KUMAR <30974908+studyskymate@users.noreply.github.com>
2020-10-23 17:50:26 +1200

    Initial commit

---
+++ src/main/java/com/dinesh/springjwt/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.dinesh.springjwt.payload.response;
+
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
